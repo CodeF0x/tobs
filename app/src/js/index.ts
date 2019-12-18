65fc1d426c85358dba4118089d16c6161ad9eaf6
CodeF0x <contact@codef0x.dev>
2019-12-18 18:51:08 +0100

    Working out layout and drog & drop.

--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -1,3 +1,14 @@
 // Because Typescript and dragula both refuse to work together, good ol' require
 const dragula = require('dragula');
-dragula([...document.getElementsByClassName('container')]);
+dragula([...document.getElementsByClassName('container')]).on(
+  'drop',
+  (
+    el: HTMLElement,
+    target: HTMLElement,
+    source: HTMLElement,
+    sibling: HTMLElement
+  ) => {
+    const toSwap: HTMLElement = target.querySelector('.dummy');
+    source.appendChild(toSwap);
+  }
+);
