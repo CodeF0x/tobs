bb3762437210679ed2cc305b7cfdf2888c43dcbe
CodeF0x <contact@codef0x.dev>
2020-01-12 22:28:45 +0100

    Implementing custom refresh-rate of charts.

--- app/src/js/preferences.js
+++ app/src/js/preferences.js
@@ -3,13 +3,24 @@
  */
 class Preferences {
   constructor() {
-    this.constants = JSON.parse(localStorage.getItem('preferences')) || {};
+    this.preferences = JSON.parse(localStorage.getItem('preferences')) || {};
     this.settings = document.getElementById('settings');
+    this.refreshRate = document.getElementById('refresh-rate');
+
+    this.refreshRate.addEventListener('change', this.saveSettings.bind(this));
+
     document
       .getElementById('settings-button')
       .addEventListener('click', this.openSettings.bind(this));
+
+    document
+      .querySelector('.fas.fa-times')
+      .addEventListener('click', this.openSettings.bind(this));
   }
 
+  /**
+   * Opens preferences overlay.
+   */
   openSettings() {
     const overlay = this.settings;
     const value = window.getComputedStyle(overlay).display;
@@ -22,6 +33,15 @@ class Preferences {
     } else {
       document.body.style.overflowY = 'scroll';
     }
+  }
+
+  /**
+   * Saves preferences to local storage.
+   */
+  saveSettings() {
+    const preferences = {
+      refreshRate: this.refreshRate.value
+    };
   }
 }
 
