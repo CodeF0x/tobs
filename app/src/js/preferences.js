76e7a6798601665ee620e8fce3f4910b9da2f4e9
CodeF0x <contact@codef0x.dev>
2020-01-12 21:19:33 +0100

    Fixed CSS problem with settings overlay.

--- app/src/js/preferences.js
+++ app/src/js/preferences.js
@@ -13,8 +13,15 @@ class Preferences {
   openSettings() {
     const overlay = this.settings;
     const value = window.getComputedStyle(overlay).display;
+    let overflow = document.body.style.overflowY;
 
     overlay.style.display = value === 'none' ? 'block' : 'none';
+
+    if (overflow === '' || overflow === 'scroll') {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'scroll';
+    }
   }
 }
 
