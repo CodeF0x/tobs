5a068370c244db6c13427abed47db92df5b0595a
CodeF0x <contact@codef0x.dev>
2020-02-26 09:55:24 +0100

    Testing custom menu.

--- app/src/js/titlebar.js
+++ app/src/js/titlebar.js
@@ -3,9 +3,10 @@
  */
 class Titlebar {
   constructor(theme) {
+    const isDev = require('electron-is-dev');
     const { Titlebar, Color } = require('custom-electron-titlebar');
     const { Menu, MenuItem } = require('electron').remote;
-    
+
     const color = theme === 'dark' ? '#343436' : '#9b9b9b';
 
     this._titlebar = new Titlebar({
@@ -13,8 +14,8 @@ class Titlebar {
       icon: '../src/img/icons/icon.png'
     });
 
-     /**
-     * Because the titlebar inserts a new div that serves as the body, 
+    /**
+     * Because the titlebar inserts a new div that serves as the body,
      * this new "body" needs to have some CSS properties set so that
      * the settings overlay can work properly.
      */
@@ -26,7 +27,48 @@ class Titlebar {
      * Becuase the custom titlebar has different height on Windows and Mac,
      * this class has to correct the top-padding of the settings overlay.
      */
-    document.getElementById('settings').style.paddingTop = document.querySelector('.titlebar').style.height;
+    document.getElementById(
+      'settings'
+    ).style.paddingTop = document.querySelector('.titlebar').style.height;
+
+    if (isDev) {
+      const menu = new Menu();
+      let mainItem = undefined;
+      let secondaryItem = undefined;
+      const config = {
+        label: 'Help',
+        submenu: [
+          {
+            label: 'View FAQ',
+            click: () =>
+              require('electron').shell.openExternal(
+                'https://github.com/CodeF0x/tobs/wiki'
+              )
+          }
+        ]
+      }
+
+      if (process.platform === 'darwin') {
+        mainItem = new MenuItem({
+          label: require('electron').remote.app.getName(),
+          submenu: [
+            {
+              role: 'quit',
+              accelerator: 'Cmd+Q'
+            }
+          ]
+        });
+
+        secondaryItem = new MenuItem(config);
+      }
+
+      menu.append(mainItem);
+      if (secondaryItem) {
+        menu.append(secondaryItem);
+      }
+
+      this._titlebar.updateMenu(menu);
+    }
   }
 }
 
